7fb1033edbba3ec97230fb4276125c856c2190ef
hjilklong <106713736+hjilklong@users.noreply.github.com>
2023-05-21 22:53:06 +0700

    Add files via upload

---
+++ UI/tempCodeRunnerFile.js
@@ -0,0 +1,28 @@
+    // $.getJSON("https://io.adafruit.com/api/v2/hjilklong/feeds/bbc-gas", function (data) {
+    //     console.log(data.last_value);
+    //     document.getElementById("gas").innerHTML = data.last_value
+    // })
+    // $.getJSON("https://io.adafruit.com/api/v2/hjilklong/feeds/door", function (data) {
+    //     console.log(data.last_value);
+    //     if (data.last_value == "OPEN")
+    //         document.getElementById("door").checked = true;
+    //     else document.getElementById("door").checked = false;
+    // })
+    // $.getJSON("https://io.adafruit.com/api/v2/hjilklong/feeds/bbc-led", function (data) {
+    //     console.log(data.last_value);
+    //      if (data.last_value == "ON")
+    //         document.getElementById("light").checked = true;
+    //     else document.getElementById("light").checked = false;
+    // })
+    // $.getJSON("https://io.adafruit.com/api/v2/hjilklong/feeds/computer", function (data) {
+    //     console.log(data.last_value);
+    //      if (data.last_value == "ON")
+    //         document.getElementById("computer").checked = true;
+    //     else document.getElementById("computer").checked = false;
+    // })
+    // $.getJSON("https://io.adafruit.com/api/v2/hjilklong/feeds/router", function (data) {
+    //     console.log(data.last_value);
+    //      if (data.last_value == "ON")
+    //         document.getElementById("router").checked = true;
+    //     else document.getElementById("router").checked = false;
+    // })
